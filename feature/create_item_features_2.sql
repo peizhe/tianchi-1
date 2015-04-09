76799ae533e3ef713f7048e0fbe879df0835875c
Shuai_Huang <shuaihuang.sjtu@gmail.com>
2015-04-09 21:15:02 +0800

    date point feature

--- feature/create_item_features_2.sql
+++ feature/create_item_features_2.sql
@@ -1,21 +1,21 @@
 # 双12期间平均每天购买次数
 drop table if exists feature.item_d12_buy;
 create table feature.item_d12_buy as
-	select item_id,(count(*)/2) as buy from washed.tianchi_p_1_30
+    select item_id,(count(*)/2) as buy from washed.tianchi_p_1_30
     where event_date between 24 and 25 and behavior_type=4
     group by item_id;
 
 # 双12期间平均每天点击次数    
 drop table if exists feature.item_d12_clk;
 create table feature.item_d12_clk as
-	select item_id,(count(*)/2) as clk from washed.tianchi_p_1_30
+    select item_id,(count(*)/2) as clk from washed.tianchi_p_1_30
     where event_date between 24 and 25 and behavior_type=1
     group by item_id;
 
 # 双12期间平均每天交互次数    
 drop table if exists feature.item_d12_act;
 create table feature.item_d12_act as
-	select item_id,(count(*)/2) as act from washed.tianchi_p_1_30
+    select item_id,(count(*)/2) as act from washed.tianchi_p_1_30
     where event_date between 24 and 25
     group by item_id;
     
@@ -24,27 +24,27 @@ create table feature.item_d12_act as
 # 双12期间人均购买次数
 drop table if exists feature.item_d12_ave_buy;
 create table feature.item_d12_ave_buy as
-	select item_id, (count(*)/count(distinct(user_id))) as ave_buy from washed.tianchi_p_1_30
+    select item_id, (count(*)/count(distinct(user_id))) as ave_buy from washed.tianchi_p_1_30
     where event_date between 24 and 25 and behavior_type=4
     group by item_id;
     
 # 双12期间人均点击次数
 drop table if exists feature.item_d12_ave_clk;
 create table feature.item_d12_ave_clk as
-	select item_id, (count(*)/count(distinct(user_id))) as ave_clk from washed.tianchi_p_1_30
+    select item_id, (count(*)/count(distinct(user_id))) as ave_clk from washed.tianchi_p_1_30
     where event_date between 24 and 25 and behavior_type=1
     group by item_id;
     
 # 双12期间人均交互次数
 drop table if exists feature.item_d12_ave_act;
 create table feature.item_d12_ave_act as
-	select item_id, (count(*)/count(distinct(user_id))) as ave_act from washed.tianchi_p_1_30
+    select item_id, (count(*)/count(distinct(user_id))) as ave_act from washed.tianchi_p_1_30
     where event_date between 24 and 25
     group by item_id;
     
 # ===================================================================
 # 第1周日均购买量
-drop table if exists feature.train_1_7_buy;
+drop table if exists feature.train_item_1_7_buy;
 create table feature.train_1_7_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -54,7 +54,7 @@ create table feature.train_1_7_buy as
     on a.item_id=b.item_id_1;
 
 # 第1周日均交互量
-drop table if exists feature.train_1_7_act;
+drop table if exists feature.train_item_1_7_act;
 create table feature.train_1_7_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -64,7 +64,7 @@ create table feature.train_1_7_act as
     on a.item_id=b.item_id_1;
 
 # 第1周人均购买量
-drop table if exists feature.train_1_7_ave_buy;
+drop table if exists feature.train_item_1_7_ave_buy;
 create table feature.train_1_7_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.train_1_7_buy) as a
@@ -74,7 +74,7 @@ create table feature.train_1_7_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第1周人均交互量
-drop table if exists feature.train_1_7_ave_act;
+drop table if exists feature.train_item_1_7_ave_act;
 create table feature.train_1_7_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.train_1_7_act) as a
@@ -84,7 +84,7 @@ create table feature.train_1_7_ave_act as
     on a.item_id=b.item_id_1;
     
 # 第2周日均购买量
-drop table if exists feature.train_8_14_buy;
+drop table if exists feature.train_item_8_14_buy;
 create table feature.train_8_14_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -94,7 +94,7 @@ create table feature.train_8_14_buy as
     on a.item_id=b.item_id_1;
 
 # 第2周日均交互量
-drop table if exists feature.train_8_14_act;
+drop table if exists feature.train_item_8_14_act;
 create table feature.train_8_14_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -104,7 +104,7 @@ create table feature.train_8_14_act as
     on a.item_id=b.item_id_1;
 
 # 第2周人均购买量
-drop table if exists feature.train_8_14_ave_buy;
+drop table if exists feature.train_item_8_14_ave_buy;
 create table feature.train_8_14_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.train_8_14_buy) as a
@@ -114,7 +114,7 @@ create table feature.train_8_14_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第2周人均交互量
-drop table if exists feature.train_8_14_ave_act;
+drop table if exists feature.train_item_8_14_ave_act;
 create table feature.train_8_14_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.train_8_14_act) as a
@@ -124,7 +124,7 @@ create table feature.train_8_14_ave_act as
     on a.item_id=b.item_id_1;
     
 # 第3周日均购买量
-drop table if exists feature.train_15_21_buy;
+drop table if exists feature.train_item_15_21_buy;
 create table feature.train_15_21_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -134,7 +134,7 @@ create table feature.train_15_21_buy as
     on a.item_id=b.item_id_1;
 
 # 第3周日均交互量
-drop table if exists feature.train_15_21_act;
+drop table if exists feature.train_item_15_21_act;
 create table feature.train_15_21_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -144,7 +144,7 @@ create table feature.train_15_21_act as
     on a.item_id=b.item_id_1;
 
 # 第3周人均购买量
-drop table if exists feature.train_15_21_ave_buy;
+drop table if exists feature.train_item_15_21_ave_buy;
 create table feature.train_15_21_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.train_15_21_buy) as a
@@ -154,7 +154,7 @@ create table feature.train_15_21_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第3周人均交互量
-drop table if exists feature.train_15_21_ave_act;
+drop table if exists feature.train_item_15_21_ave_act;
 create table feature.train_15_21_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.train_15_21_act) as a
@@ -164,7 +164,7 @@ create table feature.train_15_21_ave_act as
     on a.item_id=b.item_id_1;
     
 # 第4周日均购买量
-drop table if exists feature.train_22_30_buy;
+drop table if exists feature.train_item_22_30_buy;
 create table feature.train_22_30_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -174,7 +174,7 @@ create table feature.train_22_30_buy as
     on a.item_id=b.item_id_1;
 
 # 第4周日均交互量
-drop table if exists feature.train_22_30_act;
+drop table if exists feature.train_item_22_30_act;
 create table feature.train_22_30_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -184,7 +184,7 @@ create table feature.train_22_30_act as
     on a.item_id=b.item_id_1;
 
 # 第4周人均购买量
-drop table if exists feature.train_22_30_ave_buy;
+drop table if exists feature.train_item_22_30_ave_buy;
 create table feature.train_22_30_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.train_22_30_buy) as a
@@ -194,7 +194,7 @@ create table feature.train_22_30_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第4周人均交互量
-drop table if exists feature.train_22_30_ave_act;
+drop table if exists feature.train_item_22_30_ave_act;
 create table feature.train_22_30_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.train_22_30_act) as a
@@ -206,7 +206,7 @@ create table feature.train_22_30_ave_act as
 # =======================================================================
 
 # 第1周日均购买量
-drop table if exists feature.pre_2_8_buy;
+drop table if exists feature.pre_item_2_8_buy;
 create table feature.pre_2_8_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -216,7 +216,7 @@ create table feature.pre_2_8_buy as
     on a.item_id=b.item_id_1;
 
 # 第1周日均交互量
-drop table if exists feature.pre_2_8_act;
+drop table if exists feature.pre_item_2_8_act;
 create table feature.pre_2_8_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -226,7 +226,7 @@ create table feature.pre_2_8_act as
     on a.item_id=b.item_id_1;
 
 # 第1周人均购买量
-drop table if exists feature.pre_2_8_ave_buy;
+drop table if exists feature.pre_item_2_8_ave_buy;
 create table feature.pre_2_8_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.pre_2_8_buy) as a
@@ -236,7 +236,7 @@ create table feature.pre_2_8_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第1周人均交互量
-drop table if exists feature.pre_2_8_ave_act;
+drop table if exists feature.pre_item_2_8_ave_act;
 create table feature.pre_2_8_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.pre_2_8_act) as a
@@ -246,7 +246,7 @@ create table feature.pre_2_8_ave_act as
     on a.item_id=b.item_id_1;
     
 # 第2周日均购买量
-drop table if exists feature.pre_9_15_buy;
+drop table if exists feature.pre_item_9_15_buy;
 create table feature.pre_9_15_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -256,7 +256,7 @@ create table feature.pre_9_15_buy as
     on a.item_id=b.item_id_1;
 
 # 第2周日均交互量
-drop table if exists feature.pre_9_15_act;
+drop table if exists feature.pre_item_9_15_act;
 create table feature.pre_9_15_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -266,7 +266,7 @@ create table feature.pre_9_15_act as
     on a.item_id=b.item_id_1;
 
 # 第2周人均购买量
-drop table if exists feature.pre_9_15_ave_buy;
+drop table if exists feature.pre_item_9_15_ave_buy;
 create table feature.pre_9_15_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.pre_9_15_buy) as a
@@ -276,7 +276,7 @@ create table feature.pre_9_15_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第2周人均交互量
-drop table if exists feature.pre_9_15_ave_act;
+drop table if exists feature.pre_item_9_15_ave_act;
 create table feature.pre_9_15_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.pre_9_15_act) as a
@@ -286,7 +286,7 @@ create table feature.pre_9_15_ave_act as
     on a.item_id=b.item_id_1;
     
 # 第3周日均购买量
-drop table if exists feature.pre_16_22_buy;
+drop table if exists feature.pre_item_16_22_buy;
 create table feature.pre_16_22_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -296,7 +296,7 @@ create table feature.pre_16_22_buy as
     on a.item_id=b.item_id_1;
 
 # 第3周日均交互量
-drop table if exists feature.pre_16_22_act;
+drop table if exists feature.pre_item_16_22_act;
 create table feature.pre_16_22_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -306,7 +306,7 @@ create table feature.pre_16_22_act as
     on a.item_id=b.item_id_1;
 
 # 第3周人均购买量
-drop table if exists feature.pre_16_22_ave_buy;
+drop table if exists feature.pre_item_16_22_ave_buy;
 create table feature.pre_16_22_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.pre_16_22_buy) as a
@@ -316,7 +316,7 @@ create table feature.pre_16_22_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第3周人均交互量
-drop table if exists feature.pre_16_22_ave_act;
+drop table if exists feature.pre_item_16_22_ave_act;
 create table feature.pre_16_22_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.pre_16_22_act) as a
@@ -326,7 +326,7 @@ create table feature.pre_16_22_ave_act as
     on a.item_id=b.item_id_1;
     
 # 第4周日均购买量
-drop table if exists feature.pre_23_31_buy;
+drop table if exists feature.pre_item_23_31_buy;
 create table feature.pre_23_31_buy as
     select item_id,(sum/7) as buy 
     from (select item_id from washed.item_subset) as a
@@ -336,7 +336,7 @@ create table feature.pre_23_31_buy as
     on a.item_id=b.item_id_1;
 
 # 第4周日均交互量
-drop table if exists feature.pre_23_31_act;
+drop table if exists feature.pre_item_23_31_act;
 create table feature.pre_23_31_act as
     select item_id,(sum/7) as act 
     from (select item_id from washed.item_subset) as a
@@ -346,7 +346,7 @@ create table feature.pre_23_31_act as
     on a.item_id=b.item_id_1;
 
 # 第4周人均购买量
-drop table if exists feature.pre_23_31_ave_buy;
+drop table if exists feature.pre_item_23_31_ave_buy;
 create table feature.pre_23_31_ave_buy as
     select item_id, ifnull(buy/user,0) as ave_buy 
     from (select item_id,buy from feature.pre_23_31_buy) as a
@@ -356,7 +356,7 @@ create table feature.pre_23_31_ave_buy as
     on a.item_id=b.item_id_1;
 
 # 第4周人均交互量
-drop table if exists feature.pre_23_31_ave_act;
+drop table if exists feature.pre_item_23_31_ave_act;
 create table feature.pre_23_31_ave_act as
     select item_id, ifnull(act/user,0) as ave_act 
     from (select item_id,act from feature.pre_23_31_act) as a
---
+++ feature/create_item_features_3.sql
@@ -0,0 +1,79 @@
+# 商品第一次被购买的日期
+drop table if exists feature.train_item_first_buy;
+create table feature.train_item_first_buy as
+	select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, min(event_date) as buy_date from washed.tianchi_p_1_30
+    where behavior_type=4 group by item_id_1) as b
+    on a.item_id=b.item_id_1;
+    
+# 商品最后一次被购买的日期
+drop table if exists feature.train_item_last_buy;
+create table feature.train_item_last_buy as
+	select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, max(event_date) as buy_date from washed.tianchi_p_1_30
+    where behavior_type=4 group by item_id_1) as b
+    on a.item_id=b.item_id_1;
+    
+# 商品第一次交互的日期
+drop table if exists feature.train_item_first_act;
+create table feature.train_item_first_act as
+	select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, min(event_date) as buy_date from washed.tianchi_p_1_30
+    group by item_id_1) as b
+    on a.item_id=b.item_id_1;
+    
+# 商品最后一次交互的日期
+drop table if exists feature.train_item_last_act;
+create table feature.train_item_last_act as
+	select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, max(event_date) as buy_date from washed.tianchi_p_1_30
+    group by item_id_1) as b
+    on a.item_id=b.item_id_1;
+    
+# 商品第一次被购买的日期
+drop table if exists feature.pre_item_first_buy;
+create table feature.pre_item_first_buy as
+    select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, min(event_date) as buy_date from washed.tianchi_p_2_31
+    where behavior_type=4 group by item_id_1) as b
+    on a.item_id=b.item_id_1;
+    
+# 商品最后一次被购买的日期
+drop table if exists feature.pre_item_last_buy;
+create table feature.pre_item_last_buy as
+    select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, max(event_date) as buy_date from washed.tianchi_p_2_31
+    where behavior_type=4 group by item_id_1) as b
+    on a.item_id=b.item_id_1;
+    
+# 商品第一次交互的日期
+drop table if exists feature.pre_item_first_act;
+create table feature.pre_item_first_act as
+    select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, min(event_date) as buy_date from washed.tianchi_p_2_31
+    group by item_id_1) as b
+    on a.item_id=b.item_id_1;
+    
+# 商品最后一次交互的日期
+drop table if exists feature.pre_item_last_act;
+create table feature.pre_item_last_act as
+    select item_id,ifnull(buy_date,0) from
+    (select item_id from washed.item_subset) as a
+    left outer join
+    (select item_id as item_id_1, max(event_date) as buy_date from washed.tianchi_p_2_31
+    group by item_id_1) as b
+    on a.item_id=b.item_id_1;
