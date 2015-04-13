6bea5b1d273a4363896ec9c4462df1dd07ed9760
Shuai_Huang <shuaihuang.sjtu@gmail.com>
2015-04-13 14:24:57 +0800

    Revise createResult.py

--- feature/createResult.py
+++ feature/createResult.py
@@ -1,14 +1,5 @@
 import MySQLdb
 
-def convertTup2List(Tup):
-    List=[]
-    for TupElem in Tup:
-        List.append(TupElem[0])
-    if List!=[]:
-        return List
-    else:
-        return None
-
 Database=MySQLdb.connect(
     "202.120.37.201",
     "admin",
@@ -16,25 +7,24 @@ Database=MySQLdb.connect(
     "tianchi")
 Cursor=Database.cursor()
 
-SQL="select * from washed.item_subset;"
-Cursor.execute(SQL)
-ItemTup=Cursor.fetchall()
-SQL="select distinct user_id from washed.tianchi_p;"
-Cursor.execute(SQL)
-UserTup=Cursor.fetchall()
-
-ItemList=convertTup2List(ItemTup)
-UserList=convertTup2List(UserTup)
-
-SQL="drop table if exists result.label;"
-Cursor.execute(SQL)
-SQL="create table result.label(user_id int,item_id int);"
-Cursor.execute(SQL)
-
-for i in ItemList:
-    SQL="insert into result.label(user_id,item_id) values(%s,%s);"
-    Cursor.executemany(SQL,[(u,i) for u in UserList])
-    Database.commit()
-    print i
-
+for Date in range(1,32):
+    SQL="drop table if exists result.label_%d_pos;"%Date
+    Cursor.execute(SQL)
+    SQL="""create table result.label_%d_pos as
+    (select user_id, item_id, if(behavior_type=4,1,0) as label
+        from washed.tianchi_p
+        where behavior_type=4 and event_date=%d)   
+    """%(Date,Date)
+    Cursor.execute(SQL)
+    SQL="drop table if exists result.label_%d_neg;"%Date
+    Cursor.execute(SQL)
+    SQL="""create table result.label_%d_neg as
+    (select user_id , item_id, if(behavior_type=4,1,0) as label
+        from washed.tianchi_p
+        where behavior_type!=4 and event_date=%d
+        order by rand() limit 2000)   
+    """%(Date,Date)
+    Cursor.execute(SQL)
+    
+    print Date
 Database.close()
