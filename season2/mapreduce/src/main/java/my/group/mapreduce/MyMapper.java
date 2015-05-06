0fd601be992baa2c99b168b2a20c73c709d31e79
Yun Gu <geron762@qq.com>
2015-05-06 11:56:19 +0800

    fix

---
+++ season2/mapreduce/src/main/java/my/group/mapreduce/MyMapper.java
@@ -0,0 +1,65 @@
+package my.group.mapreduce;
+
+import com.aliyun.odps.data.Record;
+import com.aliyun.odps.mapred.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Mapper模板。请用真实逻辑替换模板内容
+ */
+public class MyMapper implements Mapper {
+    private Record key;
+    private Record value;
+
+    public void setup(TaskContext context) throws IOException {
+        key = context.createMapOutputKeyRecord();
+        value = context.createMapOutputValueRecord();
+//        one.setBigint(0, 1L);
+    }
+
+
+    
+    public void map(long recordNum, Record record, TaskContext context) throws IOException {
+    	  key.set("user_id",record.getString(0));
+    	  long type=record.getBigint(3);
+    	  long date=record.getBigint(5);
+    	  
+//    	  user_item last 1 day buy times
+    	  if(type==4 && date==30)
+    	  {
+    		  value.set("ui_l1_buy",1);
+    	  }
+    	  else
+    	  {
+    		  value.set("ui_l1_buy",0);
+    	  }
+    	  
+//    	  user_item last 3 days click times
+    	  if(type==1 && (date>=28 && date<=30))
+    	  {
+    		  value.set("ui_l3_clk",1);
+    	  }
+    	  else
+    	  {
+    		  value.set("ui_l3_clk",0);
+    	  }
+    	  
+//    	  user_item last 5 days action times
+    	  if(date>=26 && date<=30)
+    	  {
+    		  value.set("ui_l5_act",1);
+    	  }
+    	  else
+    	  {
+    		  value.set("ui_l5_act",0);
+    	  }
+    	  
+          context.write(key, value);
+    	  
+    }
+
+    public void cleanup(TaskContext context) throws IOException {
+
+    }
+}
