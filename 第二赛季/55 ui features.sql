fd8a7b99bfd0a18f97bbb492954477e5b5b529e4
jeykigung <jeykigung@qq.com>
2015-05-09 13:03:38 +0800

    所有的ui特征，共55个

---
+++ 第二赛季/55 ui features.sql
@@ -0,0 +1,10 @@
+ui_l1_buy,ui_l2_buy,ui_l3_buy,ui_l5_buy,ui_l7_buy,ui_l10_buy,ui_l14_buy,ui_l18_buy,
+ui_l1_clk,ui_l2_clk,ui_l3_clk,ui_l5_clk,ui_l7_clk,ui_l10_clk,ui_l14_clk,ui_l18_clk,
+ui_d12_clk,ui_d12_buy,ui_l1_cart,ui_l2_cart,ui_l3_cart,ui_l7_cart,
+ui_l7_favor,ui_l14_favor,ui_l18_favor,ui_l_buy_date,ui_l_act_date,ui_fl_buy_distance,
+ui_l3_clk2buy,ui_l7_clk2buy,ui_l14_clk2buy,ui_l3_later2buy,ui_l7_later2buy,ui_l14_later2buy,
+ui_l_actday_buy,ui_l_actday_favor,ui_l_actday_clk,
+ui_l3_h1_buy,ui_l5_h1_buy,ui_l7_h1_buy,ui_l3_h1_act,ui_l5_h1_act,ui_l7_h1_act,
+ui_f_buybefore_clk,ui_f_buybefore_favor,ui_f_buybefore_cart,
+ui_min_distance,ui_avg_distance,ui_max_distance,ui_median_distance,ui_stddev_distance,
+ui_l_act_distance,ui_l_actnextday_other_distinct_act,ui_l1_h1_only_act,ui_l3_h1_only_act
